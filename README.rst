3c21a49d399a34a0d0565a978ca013fcd4682787
Danilo Dellaquila <danilo.dellaquila@k-gigas.com>
2012-05-20 12:14:13 +0200

    Import first development version 1.0dev

--- README.rst
+++ README.rst
@@ -3,3 +3,14 @@ Plone.es Theme Package
 
 The ``plonetheme.plonees`` package is an installable
 customized theme product package for Plone.es website.
+
+`Plone.es`_ theme is based on `plonetheme.ploneit`_ theme
+and it uses the *theming & packaging* features available in
+`plone.app.theming`_ to make the theme easily available in
+`Plone 4.1`_.
+
+.. links:
+.. _`Plone.es`: https://github.com/plone-es/plonetheme.plonees
+.. _`plonetheme.ploneit`: https://github.com/plone-it/plonetheme.ploneit
+.. _`plone.app.theming`: http://pypi.python.org/pypi/plone.app.theming
+.. _`Plone 4.1`: http://pypi.python.org/pypi/Plone/4.1
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+setup(name='plonetheme.plonees',
+      version=version,
+      description="Theme for the Plone.es website",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Danilo Dellaquila',
+      author_email='ddellaquila@gmail.com',
+      url='https://github.com/plone-es/plonetheme.plonees',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          'plone.app.themingplugins',
+      ],
+      extras_require={
+        'test': ['plone.app.testing',]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+# uncomment these to re-enable support for Paster local commands
+#      setup_requires=["PasteScript"],
+#      paster_plugins=["ZopeSkel"],
+      )
