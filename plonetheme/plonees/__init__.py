e4b2bcd010af6c651c4e27aaccce9809b4f5d965
Danilo Dellaquila <danilo.dellaquila@k-gigas.com>
2012-07-15 18:26:21 +0200

    Add "Last News" portlet
        Create a separated portlet to display recent news
        with their description on the home page instead of
        overriding the Plone one.

--- plonetheme/plonees/__init__.py
+++ plonetheme/plonees/__init__.py
@@ -1,4 +1,5 @@
-  # -*- extra stuff goes here -*-
+from zope.i18nmessageid import MessageFactory
+PloneThemeMessageFactory = MessageFactory('plonetheme.plonees')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
---
+++ plonetheme/plonees/locales/README.txt
@@ -0,0 +1,8 @@
+Add here product's translations files using a zope.i18n
+based folder structure, e.g.:
+
+    locales/
+    -------/es
+    -------/--/LC_MESSAGES
+    -------/it
+    -------/--/LC_MESSAGES
